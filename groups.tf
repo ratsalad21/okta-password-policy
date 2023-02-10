4623a4bbf5ba35e93147536072d482927d7d7b43
Matt Everhart <matt.everhart@natl.com>
2023-02-10 12:53:57 -0500

    Added groups.tf - creating 3 test groups and rule

---
+++ groups.tf
@@ -0,0 +1,23 @@
+resource "okta_group" "test1" {
+  name        = "Test1"
+  description = "This is test group 1"
+}
+
+resource "okta_group" "test2" {
+  name        = "Test2"
+  description = "This is test group 2"
+}
+
+resource "okta_group" "test3" {
+  name        = "Test3"
+  description = "This is test group 3"
+}
+
+resource "okta_group_rule" "test3-rule" {
+  name              = "example"
+  status            = "ACTIVE"
+  group_assignments = [
+    "<group id>"]
+  expression_type   = "urn:okta:expression:1.0"
+  expression_value  = "isMemberOfGroup("okta_group.test1.id")"
+}
