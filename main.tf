2563b1b77e4bfd420a84f005fafd7737c41850af
Matt Everhart <matt.everhart@natl.com>
2023-02-10 12:20:37 -0500

    Added data source for everyone group

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
+data "okta_group" "everyone" {
+  name = "Everyone"
+}
+
 resource "okta_policy_password" "test-policy" {
   name                   = "Test Policy"
   status                 = "ACTIVE"
   description            = "Example"
   password_history_count = 4
-  groups_included        = ["${data.okta_group.everyone.id}"]
+  groups_included        = [data.okta_group.everyone.id]
 }
 
 
