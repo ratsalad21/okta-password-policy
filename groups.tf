28e8fbb476cf5e0a5fba3c45bf8d1f973d883c2f
Matt Everhart <matt.everhart@natl.com>
2023-02-10 12:58:26 -0500

    groups.tf - Added group assignement id to rule

--- groups.tf
+++ groups.tf
@@ -16,8 +16,7 @@ resource "okta_group" "test3" {
 resource "okta_group_rule" "test3-rule" {
   name              = "example"
   status            = "ACTIVE"
-  group_assignments = [
-    "${okta_group.test2.id}"]
+  group_assignments = [okta_group.test3.id]
   expression_type   = "urn:okta:expression:1.0"
   expression_value  = "isMemberOfGroup("okta_group.test1.id")"
 }
