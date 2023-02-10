aca25a0fbaa16358063e48a3bfcbcd95fc081910
Matt Everhart <matt.everhart@natl.com>
2023-02-10 11:34:33 -0500

    Initial commit to dev

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "okta_policy_password" "test policy" {
+  name                   = "Test Policy"
+  status                 = "ACTIVE"
+  description            = "Example"
+  password_history_count = 4
+  groups_included        = ["${data.okta_group.everyone.id}"]
+}
+
+
+
